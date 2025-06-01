db72d5a4b66deb802ea460103a41463b27539005
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-06-01 16:57:12 +0530

    first

---
+++ vibelyFrontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className='App text-3xl'>
+        hello world
+      </div>
+      <Button variant="contained">
+        click here
+      </Button>
+    </div>
+  )
+}
+
+export default App
