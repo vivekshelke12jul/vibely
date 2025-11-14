060121bec392bef4b092b481e21248b1d13f283b
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-11-14 20:13:01 +0530

    restarting frontend

--- vibelyFrontend/src/App.jsx
+++ vibelyFrontend/src/App.jsx
@@ -1,26 +1,35 @@
-import { Route, Routes } from "react-router";
-import "./App.css";
-import Navbar from "./components/Navbar";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Cart from "./pages/Cart";
-
-import InvalidRoute from "./pages/error/InvalidRoute";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
-import Message from "./pages/Message";
-import Chat from "./pages/Chat";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Login from "./pages/Login";
-import AuthRoute from "./components/AuthRoute";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-     
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="text-4xl text-amber-300 font-bold underline" >Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
